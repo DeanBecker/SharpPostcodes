549cce6970f0d340bc0ea6df085530ad5825c804
Dean Becker <dean.becker@nexusinds.com>
2017-02-14 16:50:17 +0000

    * Added naive request cache

---
+++ SharpPostcodes/Caching/IRequestCache.cs
@@ -0,0 +1,8 @@
+﻿namespace SharpPostcodes.Caching
+{
+    public interface IRequestCache
+    {
+        void CacheResponse(string forRequest, object response);
+        bool TryGetResponse<T>(string forRequest, out T cachedResponse);
+    }
+}
